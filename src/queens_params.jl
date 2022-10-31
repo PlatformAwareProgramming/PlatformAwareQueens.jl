9f7ff3e16924bb10faa80e8926c659790a1035cc
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-10-31 09:26:04 -0400

    set heap for all gpus

--- src/queens_params.jl
+++ src/queens_params.jl
@@ -2,7 +2,7 @@
 # Licensed under the MIT License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-global const cutoff_depth = Ref{Integer}(3)
+global const cutoff_depth = Ref{Integer}(5)
 
 function setCutoffDepth(v)
    cutoff_depth[] = v 
@@ -32,16 +32,16 @@ function getBlockSize()
     __BLOCK_SIZE_[]
 end
 
-function configureHeap()
-	
-	for gpus in  1:length(CUDA.devices())
-		@info("Setting heap on device $(gpus-1)");
-		device!(gpus-1)
-		synchronize()
-		CUDA.@check @ccall CUDA.libcudart().cudaDeviceSetLimit(CUDA.cudaLimitMallocHeapSize::CUDA.cudaLimit, 1000000000::Csize_t)::CUDA.cudaError_t
-		synchronize()
-	end
-end
+    function configureHeap()
+        
+        for gpus in  1:length(CUDA.devices())
+            @info("Setting heap on device $(gpus-1)");
+            device!(gpus-1)
+            synchronize()
+            CUDA.@check @ccall CUDA.libcudart().cudaDeviceSetLimit(CUDA.cudaLimitMallocHeapSize::CUDA.cudaLimit, 1000000000::Csize_t)::CUDA.cudaError_t
+            synchronize()
+        end
+    end
 
 export setCutoffDepth, getCutoffDepth,
        setBlockSize, getBlockSize,
